354c4f5b0b630bae08c60728b0faf2a7c6268854
bpgraham <b.graham@cs.stir.ac.uk>
2015-02-13 15:27:37 +0000

    Revert ":circus_tent: Added .gitattributes & .gitignore files"
    This reverts commit 2460167cd50400bd5c6435bec3dc72bb2c06cb94.
    
    First creation of repository
    
    Added files from scratch

---
+++ IcellWangBuzsaki.c
@@ -0,0 +1,450 @@
+/* Created by Language version: 6.2.0 */
+/* VECTORIZED */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "scoplib.h"
+#undef PI
+ 
+#include "md1redef.h"
+#include "section.h"
+#include "md2redef.h"
+
+#if METHOD3
+extern int _method3;
+#endif
+
+#undef exp
+#define exp hoc_Exp
+extern double hoc_Exp();
+ 
+#define _threadargscomma_ _p, _ppvar, _thread, _nt,
+#define _threadargs_ _p, _ppvar, _thread, _nt
+ 	/*SUPPRESS 761*/
+	/*SUPPRESS 762*/
+	/*SUPPRESS 763*/
+	/*SUPPRESS 765*/
+	 extern double *getarg();
+ /* Thread safe. No static _p or _ppvar. */
+ 
+#define t _nt->_t
+#define dt _nt->_dt
+#define drive _p[0]
+#define drivenoise _p[1]
+#define celldrive _p[2]
+#define celldrivenoise _p[3]
+#define ina _p[4]
+#define ik _p[5]
+#define il _p[6]
+#define m _p[7]
+#define n _p[8]
+#define h _p[9]
+#define Dm _p[10]
+#define Dn _p[11]
+#define Dh _p[12]
+#define minf _p[13]
+#define mtau _p[14]
+#define hinf _p[15]
+#define htau _p[16]
+#define ninf _p[17]
+#define ntau _p[18]
+#define v _p[19]
+#define _g _p[20]
+ 
+#if MAC
+#if !defined(v)
+#define v _mlhv
+#endif
+#if !defined(h)
+#define h _mlhh
+#endif
+#endif
+ static int hoc_nrnpointerindex =  -1;
+ static Datum* _extcall_thread;
+ static Prop* _extcall_prop;
+ /* external NEURON variables */
+ extern double celsius;
+ /* declaration of user functions */
+ static int _hoc_rates();
+ static int _mechtype;
+extern int nrn_get_mechtype();
+ extern void _nrn_setdata_reg(int, void(*)(Prop*));
+ static void _setdata(Prop* _prop) {
+ _extcall_prop = _prop;
+ }
+ static _hoc_setdata() {
+ Prop *_prop, *hoc_getdata_range();
+ _prop = hoc_getdata_range(_mechtype);
+   _setdata(_prop);
+ ret(1.);
+}
+ /* connect user functions to hoc names */
+ static IntFunc hoc_intfunc[] = {
+ "setdata_Icellchann", _hoc_setdata,
+ "rates_Icellchann", _hoc_rates,
+ 0, 0
+};
+ /* declare global and static user variables */
+#define el el_Icellchann
+ double el = -65;
+#define ek ek_Icellchann
+ double ek = -90;
+#define ena ena_Icellchann
+ double ena = 55;
+#define gl gl_Icellchann
+ double gl = 0.0001;
+#define gk gk_Icellchann
+ double gk = 0.009;
+#define gna gna_Icellchann
+ double gna = 0.035;
+ /* some parameters have upper and lower limits */
+ static HocParmLimits _hoc_parm_limits[] = {
+ 0,0,0
+};
+ static HocParmUnits _hoc_parm_units[] = {
+ "gna_Icellchann", "mho/cm2",
+ "ena_Icellchann", "mV",
+ "gk_Icellchann", "mho/cm2",
+ "ek_Icellchann", "mV",
+ "gl_Icellchann", "mho/cm2",
+ "el_Icellchann", "mV",
+ "drive_Icellchann", "mA/cm2",
+ "drivenoise_Icellchann", "mA/cm2",
+ "celldrive_Icellchann", "mA/cm2",
+ "celldrivenoise_Icellchann", "mA/cm2",
+ "ina_Icellchann", "mA/cm2",
+ "ik_Icellchann", "mA/cm2",
+ "il_Icellchann", "mA/cm2",
+ 0,0
+};
+ static double delta_t = 0.01;
+ static double h0 = 0;
+ static double m0 = 0;
+ static double n0 = 0;
+ /* connect global user variables to hoc */
+ static DoubScal hoc_scdoub[] = {
+ "gna_Icellchann", &gna_Icellchann,
+ "ena_Icellchann", &ena_Icellchann,
+ "gk_Icellchann", &gk_Icellchann,
+ "ek_Icellchann", &ek_Icellchann,
+ "gl_Icellchann", &gl_Icellchann,
+ "el_Icellchann", &el_Icellchann,
+ 0,0
+};
+ static DoubVec hoc_vdoub[] = {
+ 0,0,0
+};
+ static double _sav_indep;
+ static void nrn_alloc(), nrn_init(), nrn_state();
+ static void nrn_cur(), nrn_jacob();
+ 
+static int _ode_count(), _ode_map(), _ode_spec(), _ode_matsol();
+ 
+#define _cvode_ieq _ppvar[0]._i
+ /* connect range variables in _p that hoc is supposed to know about */
+ static char *_mechanism[] = {
+ "6.2.0",
+"Icellchann",
+ "drive_Icellchann",
+ "drivenoise_Icellchann",
+ 0,
+ "celldrive_Icellchann",
+ "celldrivenoise_Icellchann",
+ "ina_Icellchann",
+ "ik_Icellchann",
+ "il_Icellchann",
+ 0,
+ "m_Icellchann",
+ "n_Icellchann",
+ "h_Icellchann",
+ 0,
+ 0};
+ 
+static void nrn_alloc(_prop)
+	Prop *_prop;
+{
+	Prop *prop_ion, *need_memb();
+	double *_p; Datum *_ppvar;
+ 	_p = nrn_prop_data_alloc(_mechtype, 21, _prop);
+ 	/*initialize range parameters*/
+ 	drive = 0;
+ 	drivenoise = 0;
+ 	_prop->param = _p;
+ 	_prop->param_size = 21;
+ 	_ppvar = nrn_prop_datum_alloc(_mechtype, 1, _prop);
+ 	_prop->dparam = _ppvar;
+ 	/*connect ionic variables to this model*/
+ 
+}
+ static _initlists();
+  /* some states have an absolute tolerance */
+ static Symbol** _atollist;
+ static HocStateTolerance _hoc_state_tol[] = {
+ 0,0
+};
+ _IcellWangBuzsaki_reg() {
+	int _vectorized = 1;
+  _initlists();
+ 	register_mech(_mechanism, nrn_alloc,nrn_cur, nrn_jacob, nrn_state, nrn_init, hoc_nrnpointerindex, 1);
+ _mechtype = nrn_get_mechtype(_mechanism[1]);
+     _nrn_setdata_reg(_mechtype, _setdata);
+  hoc_register_dparam_size(_mechtype, 1);
+ 	hoc_register_cvode(_mechtype, _ode_count, _ode_map, _ode_spec, _ode_matsol);
+ 	hoc_register_tolerance(_mechtype, _hoc_state_tol, &_atollist);
+ 	hoc_register_var(hoc_scdoub, hoc_vdoub, hoc_intfunc);
+ 	ivoc_help("help ?1 Icellchann /cygdrive/c/Documents and Settings/bpg/Desktop/Projects/CortDyn/Subprojects/CA1rhythms/Code/Neuron/ThetaGamma/IcellWangBuzsaki.mod\n");
+ hoc_register_limits(_mechtype, _hoc_parm_limits);
+ hoc_register_units(_mechtype, _hoc_parm_units);
+ }
+static int _reset;
+static char *modelname = "Icellchann";
+
+static int error;
+static int _ninits = 0;
+static int _match_recurse=1;
+static _modl_cleanup(){ _match_recurse=1;}
+static rates();
+ 
+static int _ode_spec1(), _ode_matsol1();
+ static int _slist1[2], _dlist1[2];
+ static int states();
+ 
+/*CVODE*/
+ static int _ode_spec1 (double* _p, Datum* _ppvar, Datum* _thread, _NrnThread* _nt) {int _reset = 0; {
+   rates ( _threadargscomma_ v ) ;
+   Dh = 5.0 * ( hinf - h ) / htau ;
+   Dn = 5.0 * ( ninf - n ) / ntau ;
+   }
+ return _reset;
+}
+ static int _ode_matsol1 (double* _p, Datum* _ppvar, Datum* _thread, _NrnThread* _nt) {
+ rates ( _threadargscomma_ v ) ;
+ Dh = Dh  / (1. - dt*( ( (5.0)*(( ( - 1.0 ) )) ) / htau )) ;
+ Dn = Dn  / (1. - dt*( ( (5.0)*(( ( - 1.0 ) )) ) / ntau )) ;
+}
+ /*END CVODE*/
+ static int states (double* _p, Datum* _ppvar, Datum* _thread, _NrnThread* _nt) { {
+   rates ( _threadargscomma_ v ) ;
+    h = h + (1. - exp(dt*(( (5.0)*(( ( - 1.0 ) )) ) / htau)))*(- ( ( (5.0)*(( hinf )) ) / htau ) / ( ( (5.0)*(( ( - 1.0) )) ) / htau ) - h) ;
+    n = n + (1. - exp(dt*(( (5.0)*(( ( - 1.0 ) )) ) / ntau)))*(- ( ( (5.0)*(( ninf )) ) / ntau ) / ( ( (5.0)*(( ( - 1.0) )) ) / ntau ) - n) ;
+   }
+  return 0;
+}
+ 
+static int  rates ( _p, _ppvar, _thread, _nt, _lv ) double* _p; Datum* _ppvar; Datum* _thread; _NrnThread* _nt; 
+	double _lv ;
+ {
+   double _lalpha , _lbeta ;
+  _lalpha = - 0.1 * ( _lv + 35.0 ) / ( exp ( - ( _lv + 35.0 ) / 10.0 ) - 1.0 ) ;
+   _lbeta = 4.0 * exp ( - ( _lv + 60.0 ) / 18.0 ) ;
+   minf = _lalpha / ( _lalpha + _lbeta ) ;
+   _lalpha = 0.07 * exp ( - ( _lv + 58.0 ) / 20.0 ) ;
+   _lbeta = 1.0 / ( exp ( - 0.1 * ( _lv + 28.0 ) ) + 1.0 ) ;
+   htau = 1.0 / ( _lalpha + _lbeta ) ;
+   hinf = _lalpha * htau ;
+   _lalpha = 0.01 * ( _lv + 34.0 ) / ( 1.0 - exp ( - 0.1 * ( _lv + 34.0 ) ) ) ;
+   _lbeta = 0.125 * exp ( - ( _lv + 44.0 ) / 80.0 ) ;
+   ntau = 1.0 / ( _lalpha + _lbeta ) ;
+   ninf = _lalpha * ntau ;
+     return 0; }
+ 
+static int _hoc_rates() {
+  double _r;
+   double* _p; Datum* _ppvar; Datum* _thread; _NrnThread* _nt;
+   if (_extcall_prop) {_p = _extcall_prop->param; _ppvar = _extcall_prop->dparam;}else{ _p = (double*)0; _ppvar = (Datum*)0; }
+  _thread = _extcall_thread;
+  _nt = nrn_threads;
+ _r = 1.;
+ rates ( _p, _ppvar, _thread, _nt, *getarg(1) ) ;
+ ret(_r);
+}
+ 
+static int _ode_count(_type) int _type;{ return 2;}
+ 
+static int _ode_spec(_NrnThread* _nt, _Memb_list* _ml, int _type) {
+   double* _p; Datum* _ppvar; Datum* _thread;
+   Node* _nd; double _v; int _iml, _cntml;
+  _cntml = _ml->_nodecount;
+  _thread = _ml->_thread;
+  for (_iml = 0; _iml < _cntml; ++_iml) {
+    _p = _ml->_data[_iml]; _ppvar = _ml->_pdata[_iml];
+    _nd = _ml->_nodelist[_iml];
+    v = NODEV(_nd);
+     _ode_spec1 (_p, _ppvar, _thread, _nt);
+ }}
+ 
+static int _ode_map(_ieq, _pv, _pvdot, _pp, _ppd, _atol, _type) int _ieq, _type; double** _pv, **_pvdot, *_pp, *_atol; Datum* _ppd; { 
+	double* _p; Datum* _ppvar;
+ 	int _i; _p = _pp; _ppvar = _ppd;
+	_cvode_ieq = _ieq;
+	for (_i=0; _i < 2; ++_i) {
+		_pv[_i] = _pp + _slist1[_i];  _pvdot[_i] = _pp + _dlist1[_i];
+		_cvode_abstol(_atollist, _atol, _i);
+	}
+ }
+ 
+static int _ode_matsol(_NrnThread* _nt, _Memb_list* _ml, int _type) {
+   double* _p; Datum* _ppvar; Datum* _thread;
+   Node* _nd; double _v; int _iml, _cntml;
+  _cntml = _ml->_nodecount;
+  _thread = _ml->_thread;
+  for (_iml = 0; _iml < _cntml; ++_iml) {
+    _p = _ml->_data[_iml]; _ppvar = _ml->_pdata[_iml];
+    _nd = _ml->_nodelist[_iml];
+    v = NODEV(_nd);
+ _ode_matsol1 (_p, _ppvar, _thread, _nt);
+ }}
+
+static void initmodel(double* _p, Datum* _ppvar, Datum* _thread, _NrnThread* _nt) {
+  int _i; double _save;{
+  h = h0;
+  m = m0;
+  n = n0;
+ {
+   rates ( _threadargscomma_ v ) ;
+   n = ninf ;
+   h = hinf ;
+   }
+ 
+}
+}
+
+static void nrn_init(_NrnThread* _nt, _Memb_list* _ml, int _type){
+double* _p; Datum* _ppvar; Datum* _thread;
+Node *_nd; double _v; int* _ni; int _iml, _cntml;
+#if CACHEVEC
+    _ni = _ml->_nodeindices;
+#endif
+_cntml = _ml->_nodecount;
+_thread = _ml->_thread;
+for (_iml = 0; _iml < _cntml; ++_iml) {
+ _p = _ml->_data[_iml]; _ppvar = _ml->_pdata[_iml];
+#if CACHEVEC
+  if (use_cachevec) {
+    _v = VEC_V(_ni[_iml]);
+  }else
+#endif
+  {
+    _nd = _ml->_nodelist[_iml];
+    _v = NODEV(_nd);
+  }
+ v = _v;
+ initmodel(_p, _ppvar, _thread, _nt);
+}}
+
+static double _nrn_current(double* _p, Datum* _ppvar, Datum* _thread, _NrnThread* _nt, double _v){double _current=0.;v=_v;{ {
+   celldrive = - drive ;
+   celldrivenoise = - drivenoise ;
+   ina = gna * minf * minf * minf * h * ( v - ena ) ;
+   ik = gk * n * n * n * n * ( v - ek ) ;
+   il = gl * ( v - el ) ;
+   }
+ _current += ina;
+ _current += ik;
+ _current += celldrive;
+ _current += celldrivenoise;
+ _current += il;
+
+} return _current;
+}
+
+static void nrn_cur(_NrnThread* _nt, _Memb_list* _ml, int _type) {
+double* _p; Datum* _ppvar; Datum* _thread;
+Node *_nd; int* _ni; double _rhs, _v; int _iml, _cntml;
+#if CACHEVEC
+    _ni = _ml->_nodeindices;
+#endif
+_cntml = _ml->_nodecount;
+_thread = _ml->_thread;
+for (_iml = 0; _iml < _cntml; ++_iml) {
+ _p = _ml->_data[_iml]; _ppvar = _ml->_pdata[_iml];
+#if CACHEVEC
+  if (use_cachevec) {
+    _v = VEC_V(_ni[_iml]);
+  }else
+#endif
+  {
+    _nd = _ml->_nodelist[_iml];
+    _v = NODEV(_nd);
+  }
+ _g = _nrn_current(_p, _ppvar, _thread, _nt, _v + .001);
+ 	{ _rhs = _nrn_current(_p, _ppvar, _thread, _nt, _v);
+ 	}
+ _g = (_g - _rhs)/.001;
+#if CACHEVEC
+  if (use_cachevec) {
+	VEC_RHS(_ni[_iml]) -= _rhs;
+  }else
+#endif
+  {
+	NODERHS(_nd) -= _rhs;
+  }
+ 
+}}
+
+static void nrn_jacob(_NrnThread* _nt, _Memb_list* _ml, int _type) {
+double* _p; Datum* _ppvar; Datum* _thread;
+Node *_nd; int* _ni; int _iml, _cntml;
+#if CACHEVEC
+    _ni = _ml->_nodeindices;
+#endif
+_cntml = _ml->_nodecount;
+_thread = _ml->_thread;
+for (_iml = 0; _iml < _cntml; ++_iml) {
+ _p = _ml->_data[_iml];
+#if CACHEVEC
+  if (use_cachevec) {
+	VEC_D(_ni[_iml]) += _g;
+  }else
+#endif
+  {
+     _nd = _ml->_nodelist[_iml];
+	NODED(_nd) += _g;
+  }
+ 
+}}
+
+static void nrn_state(_NrnThread* _nt, _Memb_list* _ml, int _type) {
+ double _break, _save;
+double* _p; Datum* _ppvar; Datum* _thread;
+Node *_nd; double _v; int* _ni; int _iml, _cntml;
+#if CACHEVEC
+    _ni = _ml->_nodeindices;
+#endif
+_cntml = _ml->_nodecount;
+_thread = _ml->_thread;
+for (_iml = 0; _iml < _cntml; ++_iml) {
+ _p = _ml->_data[_iml]; _ppvar = _ml->_pdata[_iml];
+ _nd = _ml->_nodelist[_iml];
+#if CACHEVEC
+  if (use_cachevec) {
+    _v = VEC_V(_ni[_iml]);
+  }else
+#endif
+  {
+    _nd = _ml->_nodelist[_iml];
+    _v = NODEV(_nd);
+  }
+ _break = t + .5*dt; _save = t;
+ v=_v;
+{
+ { {
+ for (; t < _break; t += dt) {
+   states(_p, _ppvar, _thread, _nt);
+  
+}}
+ t = _save;
+ }}}
+
+}
+
+static terminal(){}
+
+static _initlists(){
+ double _x; double* _p = &_x;
+ int _i; static int _first = 1;
+  if (!_first) return;
+ _slist1[0] = &(h) - _p;  _dlist1[0] = &(Dh) - _p;
+ _slist1[1] = &(n) - _p;  _dlist1[1] = &(Dn) - _p;
+_first = 0;
+}
